585dfc073de82c353ccf97fa1bb90869c7049dfc
roger-rocha <rogerrochabnusc@gmail.com>
2023-04-30 14:04:08 -0300

    Mudando og

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,29 +6,29 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
-          <meta name="description" content="Summarize any PDF in seconds." />
+          <meta name="description" content="Resuma seu PDF em segundos." />
           <meta
             property="og:site_name"
-            content="https://pdfsummary.vercel.app/"
+            content="https://resuma-meu-pdf.vercel.app/"
           />
           <meta
             property="og:description"
-            content="Summarize any PDF in seconds."
+            content="Resuma qualquer PDF em segundos."
           />
-          <meta property="og:title" content="Summarize any PDF in seconds." />
+          <meta property="og:title" content="Resuma qualquer PDF em segundos." />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Summarize any PDF in seconds." />
+          <meta name="twitter:title" content="Resuma qualquer PDF em segundos" />
           <meta
             name="twitter:description"
-            content="Summarize any PDF in seconds."
+            content="Resuma qualquer PDF em segundos."
           />
           <meta
             property="og:image"
-            content="https://pbs.twimg.com/media/FuxomaCX0AIGbcC?format=jpg&name=small"
+            content="https://user-images.githubusercontent.com/74687838/235357148-36c1d2ae-a0ee-4c69-9372-6128d20bab50.jpeg"
           />
           <meta
             name="twitter:image"
-            content="https://pbs.twimg.com/media/FuxomaCX0AIGbcC?format=jpg&name=small"
+            content="https://user-images.githubusercontent.com/74687838/235357148-36c1d2ae-a0ee-4c69-9372-6128d20bab50.jpeg"
           />
         </Head>
         <body>
